aba791493faa90915c4210994f78d83b31bf7c02
TatianaKoba <lila-lilu@mail.ru>
2023-03-30 14:46:27 +0300

    30.03.2023 (Drawing)

---
+++ Percent/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percent
+{
+    internal class Program
+    {
+        public static double Calculate(string userInput)
+        {
+            var arr = userInput.Split();
+            var money = double.Parse(arr[0].Replace('.', ','));
+            var percent = double.Parse(arr[1].Replace('.', ','));
+            var months = Int32.Parse(arr[2]);
+
+            var incresedManey = money * Math.Pow(1 + (percent / 100) / 12, months);
+
+            return incresedManey;
+        }
+        static void Main(string[] args)
+        {
+            
+            var userInput = Console.ReadLine();
+
+            Console.WriteLine(Calculate(userInput));
+            Console.ReadKey();
+        }
+    }
+}
