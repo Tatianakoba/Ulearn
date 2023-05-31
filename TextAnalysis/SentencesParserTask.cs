b43784792e06905b70830d307c1e1e541b3789bf
TatianaKoba <lila-lilu@mail.ru>
2023-05-31 15:06:16 +0300

    31.05.2023(1)

---
+++ TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+
+            //Разделение текста на предложения. Все символы маленькие
+            string[] separatorsOfSentences = { ".", "!", "?", ";", ":", "(", ")" };
+            string[] sentences = text.ToLower().Split(separatorsOfSentences, System.StringSplitOptions.RemoveEmptyEntries);
+
+
+            //Разделение предложений на слова методом SplitIntoWords
+            foreach (string sentence in sentences)
+            {
+                //Проверка на пустую строку
+                if (!string.IsNullOrWhiteSpace(sentence) && SplitIntoWords(sentence).Capacity > 0)
+                {
+                    sentencesList.Add(SplitIntoWords(sentence));
+                }
+            }
+            return sentencesList;
+        }
+
+        //Разделение предложения на слова
+        public static List<string> SplitIntoWords(string sentence)
+        {
+            var words = new List<string>();
+            var wordsBuilder = new StringBuilder();
+
+            //Составление слов из букв и '
+            foreach (char ch in sentence)
+            {
+                if (char.IsLetter(ch) || ch == '\'')
+                    wordsBuilder.Append(ch);
+                else
+                    wordsBuilder.Append(' ');
+            }
+            string[] stringsOfWords = wordsBuilder.ToString()
+            .Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in stringsOfWords)
+            {
+                words.Add(word);
+            }
+            return words;
+        }
+    }
+}
+
---
+++ TextAnalysis/TextGeneratorTask.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class TextGeneratorTask
+    {
+        public static string ContinuePhrase(
+        Dictionary<string, string> nextWords,
+        string phraseBeginning,
+        int wordsCount)
+        {
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string[] splitWords = phraseBeginning.Split(' ');
+                if (splitWords.Length > 0)
+                {
+                    if (splitWords.Length >= 2 && nextWords.ContainsKey(splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]))
+                        phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 2] + " " + splitWords[splitWords.Length - 1]];
+
+                    else if (splitWords.Length >= 1 && nextWords.ContainsKey(splitWords[splitWords.Length - 1]))
+                        phraseBeginning += " " + nextWords[splitWords[splitWords.Length - 1]];
+
+                    else
+                        return phraseBeginning;
+                }
+            }
+
+            return phraseBeginning;
+        }
+    }
+}
+
